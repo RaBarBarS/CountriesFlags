3c25cbb440432db3eaf196286bb3fc9533fa08fb
RaBarBarS <40635641+RaBarBarS@users.noreply.github.com>
2020-09-28 09:59:49 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace PanstwaMiasta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(35, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NAZWA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(38, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "STOLICA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(37, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "POWIERZCHNIA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(38, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "LUDNOŚĆ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(38, 96);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 25);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(42, 190);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(116, 25);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(878, 588);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(38, 281);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 18);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "OD";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(144, 281);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(27, 18);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "DO";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(71, 274);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 25);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox4.Location = new System.Drawing.Point(177, 274);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(67, 25);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "0";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox5.Location = new System.Drawing.Point(177, 371);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(67, 25);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "0";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            this.textBox5.Leave += new System.EventHandler(this.textBox5_Leave);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox6.Location = new System.Drawing.Point(71, 371);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(67, 25);
+            this.textBox6.TabIndex = 11;
+            this.textBox6.Text = "0";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            this.textBox6.Leave += new System.EventHandler(this.textBox6_Leave);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(144, 374);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(27, 18);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "DO";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(38, 374);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 18);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "OD";
+            // 
+            // listView1
+            // 
+            this.listView1.Cursor = System.Windows.Forms.Cursors.No;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(277, 14);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(643, 564);
+            this.listView1.TabIndex = 15;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(933, 623);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace PanstwaMiasta
+{
+    public partial class Form1 : Form
+    {
+        List<Panstwo> lista_panstw = new List<Panstwo>();
+        ImageList imgList = new ImageList();
+        
+        [Serializable]
+        public class Panstwo
+        {
+            public string nazwa { get; set; }
+            public string stolica { get; set; }
+            public string powierzchinia { get; set; }
+            public string ludnosc { get; set; }
+            public Image flaga { get; set; }
+        }
+
+        bool nazwa = true;
+        bool stolica = true;
+        bool powierzchnia_od = true;
+        bool powierzchnia_do = true;
+        bool ludnosc_od = true;
+        bool ludnosc_do = true;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            read();
+            imgList.ImageSize = new Size(120, 80);
+            szukanko();
+        }
+
+
+        public bool czy_same_cyferki(TextBox textBox)
+        {
+            bool correct_value = true;
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(textBox.Text);
+            for (int i = 0; i < asciiBytes.Count(); i++)
+            {
+                if (asciiBytes[i] < 48 || asciiBytes[i] > 57)
+                {
+                    textBox.BackColor = Color.LightCoral;
+                    correct_value = false;
+                }
+                if (i == asciiBytes.Count() - 1 && correct_value)//ostatni przebieg fora
+                {
+                    textBox.BackColor = Color.White;
+                }
+            }
+            if (textBox.Text.Count() == 0)//żeby jak puste to też wróciło do białego
+            {
+                correct_value = true;
+                textBox.BackColor = Color.White;
+            }
+            if (correct_value)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool czy_same_literki(TextBox textBox)
+        {
+            bool correct_value = true;
+            byte[] asciiBytes = Encoding.ASCII.GetBytes(textBox.Text);
+            for (int i = 0; i < asciiBytes.Count(); i++)
+            {
+                if (asciiBytes[i] < 65 || asciiBytes[i] > 122 || asciiBytes[i] == 91 || asciiBytes[i] == 92 || asciiBytes[i] == 93 || asciiBytes[i] == 94 || asciiBytes[i] == 95 || asciiBytes[i] == 96)
+                {
+                    textBox.BackColor = Color.LightCoral;
+                    correct_value = false;
+                }
+                if (i == asciiBytes.Count() - 1 && correct_value)//ostatni przebieg fora
+                {
+                    textBox.BackColor = Color.White;
+                }
+            }
+
+            if (textBox.Text.Count() == 0)//żeby jak puste to też wróciło do białego
+            {
+                correct_value = true;
+                textBox.BackColor = Color.White;
+            }
+            if (correct_value)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (czy_same_literki(textBox1))
+            {
+                nazwa = true;
+                szukanko();
+            }
+            else
+            {
+                nazwa = false;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (czy_same_literki(textBox2))
+            {
+                stolica = true;
+                szukanko();
+            }
+            else
+            {
+                stolica = false;
+            }
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (czy_same_cyferki(textBox3))
+            {
+                powierzchnia_od = true;
+                szukanko();
+            }
+            else
+            {
+                powierzchnia_od = false;
+            }
+        }
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Length == 0)
+            {
+                textBox3.Text = "0";
+            }
+            szukanko();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+       {
+            if (czy_same_cyferki(textBox4))
+            {
+                powierzchnia_do = true;
+                szukanko();
+            }
+            else
+            {
+                powierzchnia_do = false;
+            }
+        }
+        private void textBox4_Leave(object sender, EventArgs e)
+        {
+            if (textBox4.Text.Length == 0)
+            {
+                textBox4.Text = "0";
+            }
+            szukanko();
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            if (czy_same_cyferki(textBox6))
+            {
+                ludnosc_od = true;
+                szukanko();
+            }
+            else
+            {
+                ludnosc_od = false;
+            }
+        }
+        private void textBox6_Leave(object sender, EventArgs e)
+        {
+            if (textBox6.Text.Length == 0)
+            {
+                textBox6.Text = "0";
+            }
+            szukanko();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (czy_same_cyferki(textBox5))
+            {
+                ludnosc_do = true;
+                szukanko();
+            }
+            else
+            {
+                ludnosc_do = false;
+            }
+        }
+        private void textBox5_Leave(object sender, EventArgs e)
+        {
+            if (textBox5.Text.Length == 0)
+            {
+                textBox5.Text = "0";
+            }
+            szukanko();
+        }
+
+        void szukanko()
+        {
+            imgList.Images.Clear();
+            int ilosc = 0;
+            if (nazwa && stolica && ludnosc_od && ludnosc_do && powierzchnia_od && powierzchnia_do)
+            {
+                if (textBox4.Text == "0" && textBox5.Text == "0")
+                {
+                    foreach (var elem in lista_panstw)
+                    {
+                        try
+                        {
+                            if (elem.nazwa.StartsWith(textBox1.Text) && elem.stolica.StartsWith(textBox2.Text) && Int32.Parse(elem.powierzchinia) > Int32.Parse(textBox3.Text) && Int32.Parse(elem.ludnosc) > Int32.Parse(textBox6.Text))
+                            {
+                                imgList.Images.Add(elem.flaga);
+                                ilosc++;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("problem z konwersją liczb");
+                        }
+                    }
+                }
+                else if (textBox4.Text == "0")
+                {
+                    foreach (var elem in lista_panstw)
+                    {
+                        try
+                        {
+                            if (elem.nazwa.StartsWith(textBox1.Text) && elem.stolica.StartsWith(textBox2.Text) && Int32.Parse(elem.powierzchinia) > Int32.Parse(textBox3.Text) && Int32.Parse(elem.ludnosc) > Int32.Parse(textBox6.Text) && Int32.Parse(elem.ludnosc) < Int32.Parse(textBox5.Text))
+                            {
+                                imgList.Images.Add(elem.flaga);
+                                ilosc++;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("problem z konwersją liczb");
+                        }
+                    }
+                }
+                else if (textBox5.Text == "0")
+                {
+                    foreach (var elem in lista_panstw)
+                    {
+                        try { 
+                            if (elem.nazwa.StartsWith(textBox1.Text) && elem.stolica.StartsWith(textBox2.Text) && Int32.Parse(elem.powierzchinia) > Int32.Parse(textBox3.Text) && Int32.Parse(elem.ludnosc) > Int32.Parse(textBox6.Text) && Int32.Parse(elem.powierzchinia) < Int32.Parse(textBox4.Text))
+                            {
+                                imgList.Images.Add(elem.flaga);
+                                ilosc++;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("problem z konwersją liczb");
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var elem in lista_panstw)
+                    {
+                        try
+                        {
+                            if (elem.nazwa.StartsWith(textBox1.Text) && elem.stolica.StartsWith(textBox2.Text) && Int32.Parse(elem.powierzchinia) > Int32.Parse(textBox3.Text) && Int32.Parse(elem.ludnosc) > Int32.Parse(textBox6.Text) && Int32.Parse(elem.ludnosc) < Int32.Parse(textBox5.Text) && Int32.Parse(elem.powierzchinia) < Int32.Parse(textBox4.Text))
+                            {
+                                imgList.Images.Add(elem.flaga);
+                                ilosc++;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("problem z konwersją liczb");
+                        }
+                    }
+                }
+            }
+            label5.Text = ilosc.ToString();
+            listView1.Items.Clear();
+            listView1.View = View.LargeIcon;
+            listView1.LargeImageList = imgList;
+
+            for (int i = 0; i < imgList.Images.Count; i++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.ImageIndex = i;
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void read()
+        {
+            try
+            {
+                try {
+                    foreach (var line in File.ReadLines("panstwa.txt"))
+                    {
+                        Panstwo temp = new Panstwo();
+                        var tempLine = line.Split(';');
+                        temp.nazwa = tempLine[0];
+                        temp.stolica = tempLine[1];
+                        temp.powierzchinia = tempLine[2];
+                        temp.ludnosc = tempLine[3];
+                        try
+                        {
+                            temp.flaga = Image.FromFile(temp.nazwa + ".jpg");
+                        }
+                        catch
+                        {
+                            temp.flaga = Image.FromFile(temp.nazwa + ".png");
+                        }
+                        lista_panstw.Add(temp);
+                    }
+                    Console.WriteLine("dane się ładnie wczytały");
+                }
+                catch(IOException ee)
+                {
+                    Console.WriteLine("dane w pliku są problematyczne");
+                }
+                
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("nie istnieje plik panstwa.txt");
+            }
+        }
+
+        
+    }
+}
